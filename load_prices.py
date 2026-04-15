14e047f88c5f1320567e7501a304aaeec89ea6b4
lisamariewolf0-cyber <lisamariewolf0@gmail.com>
2026-04-15 16:03:47 +0200

    Update load_prices.py

--- load_prices.py
+++ load_prices.py
@@ -3,7 +3,7 @@ import requests
 from datetime import datetime 
  
 from supabase import create_client 
-
+ 
 supabase = create_client( 
   os.environ["SUPABASE_URL"], 
   os.environ["SUPABASE_KEY"], 
